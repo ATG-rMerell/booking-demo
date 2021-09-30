d799a102d134085f1e56c99b7e1812518606124f
Robin Merell <robin.merell@atg.se>
2021-09-30 11:02:04 +0200

    first commit

---
+++ src/test/java/com/atg/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atg.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
