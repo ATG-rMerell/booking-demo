fa73562f9c650e1a04fc19bcbf9ea0dec2aae482
Robin Merell <robin.merell@atg.se>
2021-10-05 09:14:45 +0200

    use example queries in repository

--- src/main/java/com/atg/booking/repository/BookingRepository.java
+++ src/main/java/com/atg/booking/repository/BookingRepository.java
@@ -1,13 +1,26 @@
 package com.atg.booking.repository;
 
 import com.atg.booking.model.Booking;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Repository
 @Transactional
 public interface BookingRepository extends CrudRepository<Booking, Long> {
+    /*
+     * Example Queries, both returning the same result
+     *
+     * First one using built in JPA queries
+     * Second one using a custom query
+     * */
 
+    Optional<Booking> findBookingById(Long id);
+
+    @Query("SELECT b from Booking b where b.id = :id")
+    Optional<Booking> findById(@Param("id") Long id);
 }
